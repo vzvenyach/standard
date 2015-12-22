cf045bf43c508e8926e142c48916e735fdf29fcc
Ben Webb <ben.webb@opendataservices.coop>
2015-12-22 11:57:25 +0000

    Remove "[TOC]" lines
    (Sphinx doesn't understand these, but can generate it's own table of
    contents).

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,6 @@
 mkdir standard/docs/field_definitions
 python standard/schema/utils/make_field_definitions.py
-cp -r standard/assets build
 cd standard && sphinx-build -b dirhtml docs/en ../build/en
 # sphinx-build -b dirhtml -D language='es' standard/docs/en build/es
+
+cp -r standard/assets build
